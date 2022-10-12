e7e87f131ebb9696c4f0a211fd71a0d5a75b1f8d
Davi Tavares Frota <73137633+Davitfrota@users.noreply.github.com>
2022-10-12 00:10:15 -0300

    CYcle_Day_Night2

---
+++ Projeto/Cenas/Céu.gd
@@ -0,0 +1,9 @@
+extends Control
+
+
+func _process(delta):
+	var time = OS.get_time()
+	var TimeInSeconds = time.hour * 3600 + time.minute * 60 + time.second
+	var CurrentFrame = range_lerp(TimeInSeconds,0,86400,0,24)
+	$AnimationPlayer.play("Animation_Day_Night")
+	$AnimationPlayer.seek(CurrentFrame)
