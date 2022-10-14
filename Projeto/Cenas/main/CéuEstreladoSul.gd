9efa92df125b624a86bd48f122d955a1fb9d58f5
Davi Tavares Frota <73137633+Davitfrota@users.noreply.github.com>
2022-10-14 12:50:49 -0300

    outside.01

--- Projeto/Cenas/main/CéuEstreladoSul.gd
+++
@@ -1,8 +0,0 @@
-extends TextureRect
-
-func _process(delta):
-	var time = OS.get_time()
-	var TimeInSeconds = time.hour * 3600 + time.minute * 60 + time.second
-	var CurrentFrame = range_lerp(TimeInSeconds,0,86400,0,24)
-	$AnimationCeu.play("night")
-	$AnimationCeu.seek(CurrentFrame)
